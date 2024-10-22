00770c7ccaee293b786437d03309176cfafeffb0
Natan Angelo Seraglio <seraglio@iastate.edu>
2024-10-22 12:31:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import shutil
 from fisheye_correction import run_fisheye_correction
 from crop_background import run_crop_background
 from qr_renamer import run_rename_images
@@ -5,12 +6,15 @@ from pods_counting import run_count_pods
 from summary_table import generate_summary_table
 
 def run_all(folder_path, fisheye):
+    pods_counting_path = 'pods_counting'
+    shutil.copytree(folder_path, pods_counting_path, dirs_exist_ok=True)
+    
     if fisheye:
-        run_fisheye_correction(folder_path)
-    run_crop_background(folder_path)
-    run_rename_images(folder_path)
-    run_count_pods(folder_path)
-    generate_summary_table(folder_path)
+        run_fisheye_correction(pods_counting_path)
+    run_crop_background(pods_counting_path)
+    run_rename_images(pods_counting_path)
+    run_count_pods(pods_counting_path)
+    generate_summary_table(pods_counting_path)
 
 folder_path = 'yield_components'
 run_all(folder_path, fisheye=True)
