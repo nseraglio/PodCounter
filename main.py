3bd0efd5fb5a55624eec725517f0be33e8c7e3f6
n-seraglio <seraglio@iastate.edu>
2024-11-04 10:12:59 -0600

    Add grain counting

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from fisheye_correction import run_fisheye_correction
 from crop_background import run_crop_background
 from qr_renamer import run_rename_images
 from pods_counting import run_count_pods
+from grains_counting import detect_and_count_grains
 from summary_table import generate_summary_table
 
-def run_all(folder_path, fisheye):
+def run_pods(folder_path, fisheye):
     pods_counting_path = 'pods_counting'
     shutil.copytree(folder_path, pods_counting_path, dirs_exist_ok=True)
     
@@ -16,5 +17,16 @@ def run_all(folder_path, fisheye):
     run_count_pods(pods_counting_path)
     generate_summary_table(pods_counting_path)
 
-folder_path = 'yield_components'
-run_all(folder_path, fisheye=False)
+def run_grains(folder_path, fisheye):
+    grain_counting_path = 'grain_counting'
+    shutil.copytree(folder_path, grain_counting_path, dirs_exist_ok=True)
+    
+    if fisheye:
+        run_fisheye_correction(grain_counting_path)
+    run_rename_images(grain_counting_path)
+    run_crop_background(grain_counting_path)
+    detect_and_count_grains(grain_counting_path)
+    #generate_summary_table(grain_counting_path)
+
+folder_path = 'grains'
+run_grains(folder_path, fisheye=True)
